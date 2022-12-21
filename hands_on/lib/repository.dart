072fafadfda69a84e7c3a1a083d2213f0d565b95
Alliana Marie <allianaonate@gmail.com>
2022-12-21 14:20:39 +0800

    hands on

---
+++ hands_on/lib/repository.dart
@@ -0,0 +1,15 @@
+import 'package:hands_on/todo.dart';
+
+abstract class Repository{
+  
+  Future<List<Todo>> getTodoList();
+
+  Future<String> patchCompleted(Todo todo);
+
+  Future<String> putCompleted(Todo todo);
+
+  Future<String> deletedCompleted(Todo todo);
+
+  Future<String> postCompleted(Todo todo);
+
+}
